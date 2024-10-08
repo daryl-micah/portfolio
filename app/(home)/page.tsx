48f9b028ce994e65a5251e1c044c3ecac49be1e9
Daryl <darylmicah12@gmail.com>
2024-10-08 12:59:04 +0530

    added skills section

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,13 +1,17 @@
 import { Header } from "@/components/core/header";
 import Hero from "@/components/core/hero-section";
+import { Skills } from "@/components/core/skills";
 import React from "react";
 
 export default function Page() {
   return (
-    <div className="min-h-screen bg-black dark:bg-black bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2]">
+    <div className="min-h-screen bg-black dark:bg-black dark:bg-grid-white/[0.03] bg-grid-black/[0.2]">
       <div className="max-w-7xl mx-auto p-5 overflow-hidden">
         <Header />
         <Hero />
+      </div>
+      <div>
+        <Skills />
       </div>
     </div>
   );
